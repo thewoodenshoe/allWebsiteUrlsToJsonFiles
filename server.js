3939f5e745738c362373fd202834878e7069266b
stewartpa <stewartpa@cofc.edu>
2017-12-08 14:35:01 -0500

    all set but parameters parse and write json

--- server.js
+++ server.js
@@ -10,51 +10,74 @@ url = 'http://www.fordservicecontent.com/Ford_Content/vdirsnet/OwnerManual/Home/
 app.set('view engine', 'ejs') // set the view engine to ejs // set app settings
 app.use(express.static(__dirname + '/public')); // for css file
 var parsedResults = [];
+var fetchedHtml = null
 
 // Default
 app.get('/', function(req, res) {
    table = ''
+   parsedResults = [];
    res.render('index', {table})
 })
 
-app.post('/step1', function(req, res){
+app.post('/step1', function(req, res) {
+   //const url = req.body.frmUrl
    request(url, function(error, response, html){
       if(error){
          console.log(error)
-         res.end(error)
-     } else {
-         var $ = cheerio.load(html)
-         $('li','ul').each(function(i, element){
-            let data = $(this)
-            let text = data.children().text()
-            let href = "http://www.fordservicecontent.com" +data.children().attr('href')
-            parsedResults.push({ 'nr': i, 'link name': text, 'url': href })
-         })
-         let tmp = JSON.stringify(parsedResults)
-         const jsonObj = JSON.parse(tmp)
-         let table = tableify(jsonObj)
+         table = error
          res.render('index', {table})
-      } 
+         table = ''
+      } else {
+         fetchedHtml = html
+         table = fetchedHtml
+         res.render('index',{table})
+         table = ''
+      }
    })
 })
 
 app.post('/step2', function(req, res){
-  var i = 0
+   var $ = cheerio.load(fetchedHtml)
+   //const tag1 = req.body.frmTag1
+   //const tag2 = req.body.frmTag2
+   //const tag3 = req.body.frmTag3
+   $('li','ul').each(function(i, element){
+      let data = $(this)
+      let text = data.children().text()
+      //let href = tag3 +data.children().attr('href')
+      let href = "http://www.fordservicecontent.com" +data.children().attr('href')
+      parsedResults.push({ 'nr': i, 'name': text, 'url': href })
+   })
+   let tmp = JSON.stringify(parsedResults)
+   const jsonObj = JSON.parse(tmp)
+   let table = tableify(jsonObj)
+   res.render('index', {table})
+})
+
+app.post('/step3', function(req, res){
   parsedResults.forEach(function(value){
-     i += 1
      request(value.url, function(error, response, html) {
         if(error){
            console.log(error)
-           res.end(error)
+           table = error
+           res.render('index', {table})
+           table = ''
         } else {
-          var $ = cheerio.load(html)
-          fs.writeFile('./tmp/'+i+'.html',html,function(err){
-             console.log(`File ${i} was written succesfully to ./tmp/${i}.html`)
-          })
+           var $ = cheerio.load(html)
+           var directory = `./tmp/${value.name}.html`
+           // TO DO:
+           // Now create your JSON format from the html page and store it.
+           fs.writeFile(directory, html, function(error){
+              if(error){ 
+                 console.log(error)
+              } else {
+                 console.log(`File ${value.name} was written succesfully to ${directory}`)
+              }
+           })
         }
      })
   })
-  res.end()
+  res.end('Done. Check filesystem')
 })
 
 app.listen('3000')
